85f6fa58b02979c2dfc8bd48ece9b59ca3943cc9
Azrayse <114365143+Azrayse@users.noreply.github.com>
2022-11-10 22:04:23 +0100

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,10 @@
+/* Ecrivez une fonction qui prend deux paramètres et qui retourne la somme des deux paramètres. Votre fonction devra se nommer add */
+
+function add(nb1,nb2){
+    return nb1+nb2
+}
+
+// Ne touchez pas les lignes en dessous.
+module.exports = {
+  add
+}
---
+++ ex02.js
@@ -0,0 +1,10 @@
+/* Ecrivez une fonction multiply qui prend deux paramètres et retourne la multiplication des deux paramètres */
+
+function multiply (nb1,nb2) {
+return nb1*nb2
+}
+
+// Ne touchez pas les lignes en dessous.
+module.exports = {
+  multiply
+}
